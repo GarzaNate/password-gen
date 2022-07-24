7f6af37421b703e7f0bb591b18ecdef902ad9a2b
slendyG <josuegarza0608@gmail.com>
2022-07-24 16:30:34 -0700

    made changes to js file, added for-loop

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 // Assignment Code
+// Grabbing anything attatched to 'generate' class
 var generateBtn = document.querySelector("#generate");
+// Declaring var to be used to generate password
+var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()abcdefghijklmnopqrstuvwxyz';
+var passwordLength = 12;
+var password = "";
+
+for (var i = 0; i <= passwordLength; i++) {
+  var randomNumber = Math.floor(Math.random() * chars.length);
+  password += chars.substring(randomNumber, randomNumber +1);
+}
 
 // Write password to the #password input
+
 function writePassword() {
-  var password = generatePassword();
+  // For-loop to generate random length + characters
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+  }
+
+  //var textInput = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
