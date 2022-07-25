c4625676b6ebe14e95680900fc50f0afbb136d65
slendyG <josuegarza0608@gmail.com>
2022-07-24 17:48:31 -0700

    Made more changes to js file, working on prompting user criteria

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 // Assignment Code
 // Grabbing anything attatched to 'generate' class
 var generateBtn = document.querySelector("#generate");
+// Preventing browser default
+generateBtn.addEventListener("click", function(event) {
+  event.preventDefault()
+});
 // Declaring var to be used to generate password
 var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()abcdefghijklmnopqrstuvwxyz';
 var passwordLength = 12;
 var password = "";
 
-for (var i = 0; i <= passwordLength; i++) {
-  var randomNumber = Math.floor(Math.random() * chars.length);
-  password += chars.substring(randomNumber, randomNumber +1);
-}
-
 // Write password to the #password input
-
 function writePassword() {
+  var userChoice = window.prompt("Enter a number 1-12:")
+  if (!userChoice) {
+    return;
+  }
   // For-loop to generate random length + characters
   for (var i = 0; i <= passwordLength; i++) {
     var randomNumber = Math.floor(Math.random() * chars.length);
