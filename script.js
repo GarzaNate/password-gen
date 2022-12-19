ba51864d5a11ef2f4165f1a3a362e8717d1386ae
slendyG <josuegarza0608@gmail.com>
2022-12-19 01:53:03 -0800

    still not generating password

--- script.js
+++ script.js
@@ -5,85 +5,79 @@ var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", function (event) {
   event.preventDefault()
 });
-// Declaring var to be used to generate password
-var charOptions = {
-  lowercase: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k' , 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'],
-  uppercase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R','S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-  numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-  unique: ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
-}
-
-// Write password to the #password input
-
-function userPrompts() {
-  length = window.prompt("Please enter a number 8-128");
-
-    if (!length) {
-      alert("Please enter a number to move on");
-      return;
-    } else if (length < 8 || length > 128){
-      alert("Must be between 8-128 characters.")
-    };
-
-  lower = confirm("Would you like to use lowercase letters?");
-  upper = confirm("Would you like to use uppercase letters?");
-  num = confirm("Would you like to use numbers?");
-  special = confirm("Would you like to use any special characters?")
-
-}
-
-var writePassword = function () {
-  userPrompts();
-
-  if (lower && upper && num && special) {
-    selection = charOptions.lowercase.concat(charOptions.uppercase, charOptions.numbers, charOptions.unique);
-    // three criteria selection
-  } else if (lower && upper && num) {
-    selection = charOptions.lowercase.concat(charOptions.uppercase, charOptions.numbers);
-  } else if (lower && upper && special) {
-    selection = charOptions.lowercase.concat(charOptions.uppercase, charOptions.unique);
-  } else if (upper && num && special) {
-    selection = charOptions.uppercase.concat(charOptions.numbers, charOptions.unique);
-    // two criteria selection 
-  } else if (lower && upper) {
-    selection = charOptions.lowercase.concat(charOptions.uppercase);
-  } else if (lower && num) {
-    selection = charOptions.lowercase.concat(charOptions.numbers);
-  } else if (lower && special) {
-    selection = charOptions.lowercase.concat(charOptions.unique);
-  } else if (upper && num) {
-    selection = charOptions.uppercase.concat(charOptions.numbers);
-  } else if (upper && special) {
-    selection = charOptions.uppercase.concat(charOptions.unique);
-  } else if (num && special) {
-    selection = charOptions.numbers.concat(charOptions.unique);
-    // one criteria selection
-  } else if (upper) {
-    selection = charOptions.uppercase;
-  } else if (num) {
-    selection = charOptions.numbers;
-  } else if (special) {
-    selection = charOptions.unique;
-  };
-
-  randomPassword = [];
-  // For-loop to generate random length + characters
-  for (var i = 0; i < userChoice; i++) {
-    var userChoice = selection[Math.floor(Math.random() * selection.length)];
-    randomPassword.push(userChoice);
-    newPassword = randomPassword.join('');
-  };
-  return;
-}
-
-  function generatePassword() {
-    var newPassword = writePassword();
-    var passwordText = document.querySelector("#password");
-    passwordText.value = newPassword;
+function generatePassword() {
+  // prompt user for password length and character options
+  var length = prompt("Please enter desired password length (8-128 characters):");
+  var includeUppercase = confirm("Include uppercase characters?");
+  var includeLowercase = confirm("Include lowercase characters?");
+  var includeSpecial = confirm("Include special characters?");
+  var includeNumbers = confirm("Include numbers?");
+  
+  // create arrays for each character type
+  var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', ':', ';', ',', '.', '<', '>', '/', '?'];
+  var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  
+  // create empty array to store character options
+  var characters = [];
+  
+  // add desired character types to characters array
+  if (includeUppercase) {
+  characters = characters.concat(uppercase);
   }
+  if (includeLowercase) {
+  characters = characters.concat(lowercase);
+  }
+  if (includeSpecial) {
+  characters = characters.concat(special);
+  }
+  if (includeNumbers) {
+  characters = characters.concat(numbers);
+  }
+  
+  // create empty string to store password
+  var password = "";
+  
+  // add random characters to password string until desired length is reached
+  for (var i = 0; i < length; i++) {
+  password += characters[Math.floor(Math.random() * characters.length)];
+  }
+  
+  // return generated password
+  return password;
+  }
+  
+  // bind generatePassword function to button click
+  document.querySelector("generate").addEventListener("click", function() {
+  document.querySelector("password").innerHTML = generatePassword();
+  });
 
 
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword);
+
+  
+
+
+// var writePassword = function () {
+//   var randomPassword = [];
+//   // For-loop to generate random length + characters
+//   for (var i = 0; i < userChoice; i++) {
+//     var userChoice = selection[Math.floor(Math.random() * selection.length)];
+//     randomPassword.push(userChoice);
+//     var newPassword = randomPassword.join('');
+//   };
+//   return newPassword;
+// }
+
+// function generatePassword() {
+//   var newPassword = writePassword();
+//   var passwordText = document.querySelector("#password");
+//   passwordText.value = newPassword;
+// }
+
+
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", generatePassword);
 
